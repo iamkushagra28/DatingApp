6c65d5399d1b723303e7ac9a9669ac16acab0cf4
iamkushagra28 <37135051+iamkushagra28@users.noreply.github.com>
2021-10-26 16:39:34 +0530

    Added user data in Angular Client

--- API/Startup.cs
+++ API/Startup.cs
@@ -35,6 +35,7 @@ namespace API
             services.AddDbContext<DataContext>(options =>{
                 options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -50,6 +51,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+            
+            app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
